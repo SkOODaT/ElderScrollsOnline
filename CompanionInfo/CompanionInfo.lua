98673947651608b7e80f56e80d571943e3653bd0
SkOODaT <8633932+SkOODaT@users.noreply.github.com>
2021-06-09 13:49:19 -0400

    Initial Add Of CompanionInfo

---
+++ CompanionInfo/CompanionInfo.lua
@@ -0,0 +1,345 @@
+-- TODO
+-- Max Level Check If Needed~ Lv 20
+-- Fix Weird Text/Companion States
+-- Hide On More States?
+-- Maybe Make Text Length Dynamic/Alt Styles?
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+local AddName = "CompanionInfo"
+local Version = 100035
+----------------------------------
+local Window = GetWindowManager()
+local CompanionInfoText = Window:CreateTopLevelWindow(nil)
+local CompanionInfoIcon = Window:CreateTopLevelWindow(nil)
+----------------------------------
+local GainedExperience = 0
+----------------------------------
+local ChangedRapport = 0
+local PrevRapport = 0
+local RAPOperator = ""
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function CIFramesSetup()
+    if not CISaved.Toggle then return end
+    -- Make Text
+    CompanionInfoText:ClearAnchors()  
+    CompanionInfoText:SetDimensions(575,15)
+    CompanionInfoText:SetAnchor(CISaved.Point,GuiRoot,nil,CISaved.X,CISaved.Y)
+    CompanionInfoText.Text = Window:CreateControl("CompanionInfoText", CompanionInfoText, CT_LABEL)
+    CompanionInfoText.Text:SetAnchorFill(CompanionInfoText)
+    CompanionInfoText.Text:SetFont("ZoFontGame")
+    -- Clamp Main Frame To Screen
+    CompanionInfoText:SetClampedToScreen(true)
+    -- Make Icon
+    CompanionInfoIcon:ClearAnchors()
+    CompanionInfoIcon:SetDimensions(25,25)
+    CompanionInfoIcon:SetAnchor(RIGHT,CompanionInfoText,LEFT,0,3)     
+    CompanionInfoIcon.Icon = Window:CreateControl("CompanionInfoIcon", CompanionInfoIcon, CT_TEXTURE)
+    CompanionInfoIcon.Icon:SetAnchorFill(CompanionInfoIcon)
+    -- Make Tooltip 
+    CompanionInfoText:SetHandler("OnMouseEnter", CIFramesOnMouseEnter)
+    CompanionInfoText:SetHandler("OnMouseExit", CIFramesOnMouseExit)
+    -- Check Lock Status
+    CILock()
+    -- Hide Frame On Setup
+    CIFramesHide()
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function CIFramesShow()
+    if not CISaved.Toggle then return end
+    CompanionInfoText:SetHidden(false)
+    CompanionInfoIcon:SetHidden(false)
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function CIFramesHide()
+    -- Reset Values To 0 On Setup/Deactivated Event And Addon Toggle
+    GainedExperience = 0
+    PrevRapport = 0
+    ChangedRapport = 0
+    CompanionInfoText:SetHidden(true)
+    CompanionInfoIcon:SetHidden(true)
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function CIFramesNoZeroHide()
+    -- No Zero Hide On Interaction Events And Temp Stuff
+    CompanionInfoText:SetHidden(true)
+    CompanionInfoIcon:SetHidden(true)
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function CICompantionInfo()
+    -- Gather Companion Infos
+    if not HasActiveCompanion() then return end
+    local NameVar = GetActiveCompanionDefId()
+    local Name = GetCompanionName(NameVar)
+    -- Trim Garbage
+    if Name:find("%^M$") then 
+        Name = Name:gsub("%^M", "")
+    elseif Name:find("%^F$") then
+        Name = Name:gsub("%^F", "")
+    end
+    local Icon = ZO_COMPANION_MANAGER:GetActiveCompanionIcon()
+    local Level, XP = GetActiveCompanionLevelInfo()
+    local TotalXP = GetNumExperiencePointsInCompanionLevel(Level + 1) or 0
+	local XPPercent = zo_floor(XP / TotalXP * 100)
+    local RapLevel = GetActiveCompanionRapportLevel()
+    local RapMin = GetMinimumRapport() 
+    local RapValue = GetActiveCompanionRapport()
+    local RapMax = GetMaximumRapport()
+    local RapLevelString = GetString("SI_COMPANIONRAPPORTLEVEL", RapLevel)
+    local RapLevelDesc = GetActiveCompanionRapportLevelDescription(GetActiveCompanionRapportLevel())
+    local PassiveAbility = GetAbilityName(GetCompanionPassivePerkAbilityId(GetActiveCompanionDefId()))
+    local PassiveAbilityDesc = GetAbilityDescription(GetCompanionPassivePerkAbilityId(GetActiveCompanionDefId()))
+    -- Set Outfit To None If Nil
+    local Outfit = "None"
+    if ZO_OUTFIT_MANAGER:GetEquippedOutfitIndex(GAMEPLAY_ACTOR_CATEGORY_COMPANION) then
+        Outfit = ZO_OUTFIT_MANAGER:GetEquippedOutfitIndex(GAMEPLAY_ACTOR_CATEGORY_COMPANION)
+    end
+    return Name, Icon, Level, XP, TotalXP, XPPercent, RapLevel, RapMin, RapValue, RapMax, RapLevelString, RapLevelDesc, PassiveAbility, PassiveAbilityDesc, Outfit
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function CIFramesOnMouseEnter()
+    -- Int Tooltip
+    InitializeTooltip(InformationTooltip, CompanionInfoText, BOTTOM, 0, -5)
+    -- Set Tooltip Info
+    local TooltipText = ""
+    if HasActiveCompanion() and not HasPendingCompanion() then
+        Name, Icon, Level, XP, TotalXP, XPPercent, RapLevel, RapMin, RapValue, RapMax, RapLevelString, RapLevelDesc, PassiveAbility, PassiveAbilityDesc, Outfit = CICompantionInfo()
+        TooltipText = "|c"..CISaved.TextColor..Name.." (Outfit "..Outfit..") |t16:16:"..Icon.."|t"..
+                      "\nLv:"..Level.." ("..XP.."/"..TotalXP..") "..XPPercent.."%".." [+"..GainedExperience.."]"..
+                      "\nRap:"..RapLevel.." ("..PrevRapport..") ("..RapMin.."/"..RapValue.."/"..RapMax..")".." ["..RAPOperator..ChangedRapport.."]"..
+                      "\n|c00FEF1"..RapLevelString.."|r: |c"..CISaved.TextColor..RapLevelDesc..
+                      "\n|c34FE00"..PassiveAbility.."|r: |c"..CISaved.TextColor..PassiveAbilityDesc..
+                      "|r"
+    end
+    -- Set Tooltip Text
+    SetTooltipText(InformationTooltip, TooltipText)
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function CIFramesOnMouseExit()
+    -- Clear Tooltip
+    ClearTooltip(InformationTooltip)
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function CIFramesBuiltFrame()
+    Name, Icon, Level, XP, TotalXP, XPPercent, RapLevel, RapMin, RapValue, RapMax, RapLevelString, RapLevelDesc, PassiveAbility, PassiveAbilityDesc, Outfit = CICompantionInfo()
+    -- Built Frame Text
+    local FrameIcon = ""
+    local FrameName = ""
+    local FrameLevel = ""
+    local FrameExpierence = ""
+    local FramePercent = ""
+    local FrameGain = ""
+    local FrameLevelRap = ""
+    local FramePrevRap = ""
+    local FrameChangedRap = ""
+    local FrameCurrentRap = ""
+    local FrameDescRap = ""
+    -- Icon
+    if CISaved.MainIcon then
+        FrameIcon = Icon
+    end
+    -- Name
+    if CISaved.MainName then
+        -- Trim Names 
+        if CISaved.MainNameShort then
+            if Name:find("% Hallix$") then 
+                Name = Name:gsub("% Hallix", "")
+            elseif Name:find("% Elendis$") then
+                Name = Name:gsub("% Elendis", "")
+            end
+        end
+        FrameName = Name
+    end
+    -- Level
+    if CISaved.MainName and CISaved.MainLevel then
+        FrameLevel = " Lv:"..Level..""
+    elseif CISaved.MainLevel then
+        FrameLevel = "Lv:"..Level..""
+    end
+    -- Raw XP
+    if CISaved.MainRawXP then 
+        FrameExpierence = " ("..XP.."/"..TotalXP..")"
+    end
+    -- Percent XP
+    if CISaved.MainPercentXP then
+        FramePercent = " "..XPPercent.."%"
+    end
+    -- Gain XP
+    if CISaved.MainGainXP then
+        FrameGain = " [+"..GainedExperience.."]"
+    end
+    -- Rap Level
+    if CISaved.MainLevelRap then
+        FrameLevelRap = " Rap:"..RapLevel..""
+    end
+    -- Rap Previous
+    if CISaved.MainPrevRap then
+        FramePrevRap = " ("..PrevRapport..")"
+    end
+    -- Rap Changed
+    if CISaved.MainChangedRap then
+        FrameChangedRap = " ["..RAPOperator..ChangedRapport.."]"
+    end
+    -- Rap Changed
+    if CISaved.MainCurrentRap then
+        FrameCurrentRap = " ("..RapValue..")"
+    end
+    -- Rap Description
+    if CISaved.MainDescRap then
+        FrameDescRap = " "..RapLevelString
+    end
+    return FrameIcon, FrameName, FrameLevel, FrameExpierence, FramePercent, FrameGain, FrameLevelRap, FramePrevRap, FrameChangedRap, FrameCurrentRap, FrameDescRap
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function CIFramesUpdate()
+    if not CISaved.Toggle then return end
+    if CompanionInfoText.Text == nil then return end
+    local FrameText = ""
+    if HasActiveCompanion() and not HasPendingCompanion() then
+        FrameIcon, FrameName, FrameLevel, FrameExpierence, FramePercent, FrameGain, FrameLevelRap, FramePrevRap, FrameChangedRap, FrameCurrentRap, FrameDescRap = CIFramesBuiltFrame()
+        -- Update Icon
+        CompanionInfoIcon.Icon:SetTexture(FrameIcon)
+        if not CISaved.MainIcon then
+            CompanionInfoIcon:SetHidden(true)
+        elseif GetInteractionType() == 14 then
+            CompanionInfoIcon:SetHidden(true)
+        else
+            CompanionInfoIcon:SetHidden(false)
+        end
+        FrameText = "|c"..CISaved.TextColor..FrameName..
+                    FrameLevel..FrameExpierence..FramePercent..FrameGain..
+                    FrameLevelRap..FramePrevRap..FrameChangedRap..FrameCurrentRap..FrameDescRap..
+                    "|r"
+    else
+        -- No Zeroed Hide
+        CIFramesNoZeroHide()
+    end
+    -- Set Frame Text
+    CompanionInfoText.Text:SetText(FrameText)
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function CIFramesXPEvent(companionId, level, previousExperience, currentExperience)
+    GainedExperience = currentExperience - previousExperience
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function CIFramesRapEvent(companionId, previousRapport, currentRapport)
+    PrevRapport = previousRapport
+    ChangedRapport = currentRapport - previousRapport
+    if currentRapport > previousRapport then
+        RAPOperator = "+"
+    end
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function CompanionWarning(warningType, companionId)
+    d("Companion Info Warning Event: "..warningType..""..companionId)
+    if warningType == COMPANION_WARNING_TYPE_REMOVED_FOR_GROUP_PLAYER then
+        d("[CompanionInfo Warning] Companion UnSummoned Due To Group Being Full.")
+    elseif warningType == COMPANION_WARNING_TYPE_SUMMON_FAILED_LOW_RAPPORT then
+        d("[CompanionInfo Warning] Companion Did NOT Summon Due To Low Rapport.")
+    else
+        d("[CompanionInfo Warning] Event Error Occured "..warningType)
+    end
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function CISavedVariables()
+    CISaved = ZO_SavedVars:New(AddName.."_SavedVariables", Version, nil, 
+    { 
+        Toggle = true,
+        X = -378,
+        Y = -3,
+        Point = 9,
+        RPoint = 9,
+        Lock = false,
+        MainIcon = true,
+        MainName = true,
+        MainNameShort = true,
+        MainLevel = true,
+        MainRawXP = true, 
+        MainPercentXP = true,
+        MainGainXP = true,
+        MainLevelRap = true,
+        MainPrevRap = true,
+        MainChangedRap = true,
+        MainCurrentRap = true,
+        MainDescRap = true,
+        TextColor = "FFFFFF",
+    })
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function CIUpdatePosition()
+    local validAnchor, point, relativeTo, relativePoint, offSetX, offSetY = CompanionInfoText:GetAnchor()
+    if(offSetX ~= CISaved.X or offSetY ~= CISaved.Y) then
+        --d(string.format("Point: %d\nPrevious Anchor: %d, %d\nNew Anchor: %d, %d\n", point, CISaved.X, CISaved.Y, offSetX, offSetY))
+        CISaved.X = offSetX
+        CISaved.Y = offSetY
+        CISaved.Point = point
+        if(relativePoint ~= nil) then
+            CISaved.RPoint = relativePoint
+        end
+    end
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function CILock()
+    if CISaved.Lock then
+        CompanionInfoText:SetMovable(false)
+        CompanionInfoText:SetMouseEnabled(false)
+    else
+        CompanionInfoText:SetMovable(true)
+        CompanionInfoText:SetMouseEnabled(true)
+    end
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function CompanionInfoOnLoad(Event, AddonName)
+    if AddonName ~= AddName then return end
+    -- Load Saved Vars
+    CISavedVariables()
+    -- Setup Frames
+    CIFramesSetup()
+    -- Show If Companion OnLoad
+    if HasActiveCompanion() then
+        CIFramesShow()
+        CIFramesUpdate()
+    end
+    -- Make Settings Panel
+    EVENT_MANAGER:RegisterForEvent(AddName.."_Player", EVENT_PLAYER_ACTIVATED,
+    function()
+    CILAM2Panel()
+        EVENT_MANAGER:UnregisterForEvent(AddName.."_Player", EVENT_PLAYER_ACTIVATED)
+    end)
+    EVENT_MANAGER:UnregisterForEvent(AddName.."_OnLoad", EVENT_ADD_ON_LOADED)
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Debug For Later
+--[[function CIFramesState(event, newState, oldState)
+    d("["..AddName.."] OLD State: "..oldState)
+    d("["..AddName.."] NEW State: "..newState)
+end
+
+HasActiveCompanion()  HasPendingCompanion()  GetPendingCompanionDefId()  
+
+EVENT_ACTIVE_COMPANION_STATE_CHANGED (CompanionState newState, CompanionState oldState) 
+CompanionState
+COMPANION_STATE_ACTIVE
+COMPANION_STATE_INACTIVE
+COMPANION_STATE_PENDING   IsOwnerOfCurrentHouse()
+]]--
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Warning Event 
+EVENT_MANAGER:RegisterForEvent(AddName.."_Warning", EVENT_COMPANION_WARNING, CompanionWarning) -- Might Of Been Renamed To Below~
+EVENT_MANAGER:RegisterForEvent(AddName.."_Warning", EVENT_COMPANION_SUMMON_RESULT, CompanionWarning)
+-- Update Event 
+EVENT_MANAGER:RegisterForEvent(AddName.."_Update", EVENT_COMPANION_EXPERIENCE_GAIN, function(eventId, ...) CIFramesXPEvent(...) CIFramesUpdate() end)
+EVENT_MANAGER:RegisterForEvent(AddName.."_Update", EVENT_COMPANION_RAPPORT_UPDATE, function(eventId, ...) CIFramesRapEvent(...) CIFramesUpdate() end)
+--EVENT_MANAGER:RegisterForEvent(AddName.."_Update", EVENT_ACTIVE_COMPANION_STATE_CHANGED, CIFramesUpdate)
+EVENT_MANAGER:RegisterForEvent(AddName.."_Update", EVENT_OPEN_COMPANION_MENU, CIFramesUpdate)
+EVENT_MANAGER:RegisterForEvent(AddName.."_Update", EVENT_RETICLE_TARGET_CHANGED, CIFramesUpdate)
+-- Show Event
+EVENT_MANAGER:RegisterForEvent(AddName.."_Show", EVENT_COMPANION_ACTIVATED, function() CIFramesShow() CIFramesUpdate() end)
+EVENT_MANAGER:RegisterForEvent(AddName.."_Show", EVENT_CHATTER_END, function() CIFramesShow() CIFramesUpdate() end)
+-- Hide/Show Event
+EVENT_MANAGER:RegisterForEvent(AddName.."_Hide", EVENT_COMPANION_DEACTIVATED, function() CIFramesHide() end)
+EVENT_MANAGER:RegisterForEvent(AddName.."_Hide", EVENT_CHATTER_BEGIN, function() CIFramesHide() end)
+-- Saved Vars Event
+EVENT_MANAGER:RegisterForEvent(AddName.."_Vars", EVENT_GLOBAL_MOUSE_DOWN, function() CIUpdatePosition() end)
+EVENT_MANAGER:RegisterForEvent(AddName.."_Vars", EVENT_GLOBAL_MOUSE_UP, function() CIUpdatePosition() end)
+-- LoadUp Event
+EVENT_MANAGER:RegisterForEvent(AddName.."_OnLoad", EVENT_ADD_ON_LOADED, CompanionInfoOnLoad)
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
---
+++ CompanionInfo/CompanionInfoMenu.lua
@@ -0,0 +1,162 @@
+local PanelTitle = "CompanionInfo"
+local Version = "100035.17"
+local Author = "SkOODaT"
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+local LAM2 = LibAddonMenu2
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function CILAM2Panel()
+    local PanelData = 
+    {
+        type = "panel",
+        name = PanelTitle,
+        author = Author,
+        version = Version,
+        slashCommand = "/cic",
+    }
+    local OptionsData = 
+    {
+        [1] =
+        {
+            type = "description",
+            text = "Simple Addon To Show Active Companion Info.",
+        },
+        [2] = 
+        {
+            type = "header",
+            name = "General"
+        },
+        [3] = 
+        {
+            type = "checkbox",
+            name = "CompanionInfo On/Off",
+            tooltip = "Toggle Overall Addon CompanionInfo On/Off.",
+            getFunc = function() return CISaved.Toggle end,
+            setFunc = function(value) CISaved.Toggle = value if CISaved.Toggle then CIFramesShow() else CIFramesHide() end end,
+        },
+        [4] = 
+        {
+            type = "checkbox",
+            name = "Lock On/Off",
+            tooltip = "Lock CompanionInfo Placement On/Off.",
+            getFunc = function() return CISaved.Lock end,
+            setFunc = function(value) CISaved.Lock = value CILock() end,
+        },
+        [5] = 
+        {
+            type = "header",
+            name = "Main Frame Settings"
+        },
+        [6] = 
+        {
+            type = "checkbox",
+            name = "Main Frame Icon On/Off",
+            tooltip = "Change CompanionInfo Main Frame Icon On/Off.",
+            getFunc = function() return CISaved.MainIcon end,
+            setFunc = function(value) CISaved.MainIcon = value CIFramesUpdate() end,
+        },
+        [7] = 
+        {
+            type = "checkbox",
+            name = "Main Frame Name On/Off",
+            tooltip = "Change CompanionInfo Main Frame Name On/Off.",
+            getFunc = function() return CISaved.MainName end,
+            setFunc = function(value) CISaved.MainName = value CIFramesUpdate() end,
+        },
+        [8] = 
+        {
+            type = "checkbox",
+            name = "Main Frame Name Short On/Off",
+            tooltip = "Change CompanionInfo Main Frame Name Short On/Off.",
+            getFunc = function() return CISaved.MainNameShort end,
+            setFunc = function(value) CISaved.MainNameShort = value CIFramesUpdate() end,
+        },
+        [9] = 
+        {
+            type = "checkbox",
+            name = "Main Frame Level On/Off",
+            tooltip = "Change CompanionInfo Main Frame Level On/Off.",
+            getFunc = function() return CISaved.MainLevel end,
+            setFunc = function(value) CISaved.MainLevel = value CIFramesUpdate() end,
+        },
+        [10] = 
+        {
+            type = "checkbox",
+            name = "Main Frame XP Raw On/Off",
+            tooltip = "Change CompanionInfo Main Frame XP Raw On/Off.",
+            getFunc = function() return CISaved.MainRawXP end,
+            setFunc = function(value) CISaved.MainRawXP = value CIFramesUpdate() end,
+        },
+        [11] = 
+        {
+            type = "checkbox",
+            name = "Main Frame XP Percent On/Off",
+            tooltip = "Change CompanionInfo Main Frame XP Percent On/Off.",
+            getFunc = function() return CISaved.MainPercentXP end,
+            setFunc = function(value) CISaved.MainPercentXP = value CIFramesUpdate() end,
+        },
+        [12] = 
+        {
+            type = "checkbox",
+            name = "Main Frame XP Gains On/Off",
+            tooltip = "Change CompanionInfo Main Frame XP Gains On/Off.",
+            getFunc = function() return CISaved.MainGainXP end,
+            setFunc = function(value) CISaved.MainGainXP = value CIFramesUpdate() end,
+        },
+        [13] = 
+        {
+            type = "checkbox",
+            name = "Main Frame Rapport Level On/Off",
+            tooltip = "Change CompanionInfo Main Frame Rap Level On/Off.",
+            getFunc = function() return CISaved.MainLevelRap end,
+            setFunc = function(value) CISaved.MainLevelRap = value CIFramesUpdate() end,
+        },
+        [14] = 
+        {
+            type = "checkbox",
+            name = "Main Frame Previous Rapport On/Off",
+            tooltip = "Change CompanionInfo Main Frame Previous Rapport On/Off.",
+            getFunc = function() return CISaved.MainPrevRap end,
+            setFunc = function(value) CISaved.MainPrevRap = value CIFramesUpdate() end,
+        },
+        [15] = 
+        {
+            type = "checkbox",
+            name = "Main Frame Changed Rapport On/Off",
+            tooltip = "Change CompanionInfo Main Frame Changed Rapport On/Off.",
+            getFunc = function() return CISaved.MainChangedRap end,
+            setFunc = function(value) CISaved.MainChangedRap = value CIFramesUpdate() end,
+        },
+        [16] = 
+        {
+            type = "checkbox",
+            name = "Main Frame Current Rapport On/Off",
+            tooltip = "Change CompanionInfo Main Frame Current Rapport On/Off.",
+            getFunc = function() return CISaved.MainCurrentRap end,
+            setFunc = function(value) CISaved.MainCurrentRap = value CIFramesUpdate() end,
+        },
+        [17] = 
+        {
+            type = "checkbox",
+            name = "Main Frame Rapport Description On/Off",
+            tooltip = "Change CompanionInfo Main Frame Rapport Description On/Off.",
+            getFunc = function() return CISaved.MainDescRap end,
+            setFunc = function(value) CISaved.MainDescRap = value CIFramesUpdate() end,
+        },
+        [18] = 
+        {
+            type = "header",
+            name = "Text Colors"
+        },
+        [19] =
+        {
+            type = "editbox",
+            name = "Text Colors",
+            tooltip = "Change All CompanionInfo Text Colors.",
+            getFunc = function() return CISaved.TextColor end,
+            setFunc = function(value) CISaved.TextColor = value CIFramesUpdate() end,
+        },
+    }   
+    LAM2:RegisterAddonPanel(PanelTitle.."LAM2Options", PanelData)
+    LAM2:RegisterOptionControls(PanelTitle.."LAM2Options", OptionsData)
+end
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
